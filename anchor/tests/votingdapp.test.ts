f58c2a15a19c482179614cf8ca131c0db3ed1a3a
ayush99336 <ayushkaushikhere@gmail.com>
2025-08-25 15:06:59 +0530

    initial contract

--- anchor/tests/votingdapp.test.ts
+++ anchor/tests/votingdapp.test.ts
@@ -16,9 +16,12 @@ import {
   getInitializeInstruction,
   getSetInstruction,
 } from '../src'
+
+import fs from 'fs'
 // @ts-ignore error TS2307 suggest setting `moduleResolution` but this is already configured
 import { loadKeypairSignerFromFile } from 'gill/node'
-
+const IDL = require('../target/idl/votingdapp.json')
+import { Votingdapp } from '../target/types/votingdapp.ts'
 const { rpc, sendAndConfirmTransaction } = createSolanaClient({ urlOrMoniker: process.env.ANCHOR_PROVIDER_URL! })
 
 describe('votingdapp', () => {
@@ -30,7 +33,7 @@ describe('votingdapp', () => {
     payer = await loadKeypairSignerFromFile(process.env.ANCHOR_WALLET!)
   })
 
-  it('Initialize Votingdapp', async () => {
+  it('Initialize votingdapp', async () => {
     // ARRANGE
     expect.assertions(1)
     const ix = getInitializeInstruction({ payer: payer, votingdapp: votingdapp })
@@ -39,87 +42,87 @@ describe('votingdapp', () => {
     await sendAndConfirm({ ix, payer })
 
     // ASSER
-    const currentVotingdapp = await fetchVotingdapp(rpc, votingdapp.address)
-    expect(currentVotingdapp.data.count).toEqual(0)
+    const currentvotingdapp = await fetchVotingdapp(rpc, votingdapp.address)
+    expect(currentvotingdapp.data.count).toEqual(0)
   })
 
-  it('Increment Votingdapp', async () => {
-    // ARRANGE
-    expect.assertions(1)
-    const ix = getIncrementInstruction({
-      votingdapp: votingdapp.address,
-    })
+  // it('Increment votingdapp', async () => {
+  //   // ARRANGE
+  //   expect.assertions(1)
+  //   const ix = getIncrementInstruction({
+  //     votingdapp: votingdapp.address,
+  //   })
 
-    // ACT
-    await sendAndConfirm({ ix, payer })
+  //   // ACT
+  //   await sendAndConfirm({ ix, payer })
 
-    // ASSERT
-    const currentCount = await fetchVotingdapp(rpc, votingdapp.address)
-    expect(currentCount.data.count).toEqual(1)
-  })
+  //   // ASSERT
+  //   const currentCount = await fetchVotingdapp(rpc, votingdapp.address)
+  //   expect(currentCount.data.count).toEqual(1)
+  // })
 
-  it('Increment Votingdapp Again', async () => {
-    // ARRANGE
-    expect.assertions(1)
-    const ix = getIncrementInstruction({ votingdapp: votingdapp.address })
+  // it('Increment votingdapp Again', async () => {
+  //   // ARRANGE
+  //   expect.assertions(1)
+  //   const ix = getIncrementInstruction({ votingdapp: votingdapp.address })
 
-    // ACT
-    await sendAndConfirm({ ix, payer })
+  //   // ACT
+  //   await sendAndConfirm({ ix, payer })
 
-    // ASSERT
-    const currentCount = await fetchVotingdapp(rpc, votingdapp.address)
-    expect(currentCount.data.count).toEqual(2)
-  })
+  //   // ASSERT
+  //   const currentCount = await fetchVotingdapp(rpc, votingdapp.address)
+  //   expect(currentCount.data.count).toEqual(2)
+  // })
 
-  it('Decrement Votingdapp', async () => {
-    // ARRANGE
-    expect.assertions(1)
-    const ix = getDecrementInstruction({
-      votingdapp: votingdapp.address,
-    })
+  // it('Decrement votingdapp', async () => {
+  //   // ARRANGE
+  //   expect.assertions(1)
+  //   const ix = getDecrementInstruction({
+  //     votingdapp: votingdapp.address,
+  //   })
 
-    // ACT
-    await sendAndConfirm({ ix, payer })
+  //   // ACT
+  //   await sendAndConfirm({ ix, payer })
 
-    // ASSERT
-    const currentCount = await fetchVotingdapp(rpc, votingdapp.address)
-    expect(currentCount.data.count).toEqual(1)
-  })
+  //   // ASSERT
+  //   const currentCount = await fetchVotingdapp(rpc, votingdapp.address)
+  //   expect(currentCount.data.count).toEqual(1)
+  // })
 
-  it('Set votingdapp value', async () => {
-    // ARRANGE
-    expect.assertions(1)
-    const ix = getSetInstruction({ votingdapp: votingdapp.address, value: 42 })
+  // it('Set votingdapp value', async () => {
+  //   // ARRANGE
+  //   expect.assertions(1)
+  //   const ix = getSetInstruction({ votingdapp: votingdapp.address, value: 42 })
 
-    // ACT
-    await sendAndConfirm({ ix, payer })
+  //   // ACT
+  //   await sendAndConfirm({ ix, payer })
 
-    // ASSERT
-    const currentCount = await fetchVotingdapp(rpc, votingdapp.address)
-    expect(currentCount.data.count).toEqual(42)
-  })
+  //   // ASSERT
+  //   const currentCount = await fetchVotingdapp(rpc, votingdapp.address)
+  //   expect(currentCount.data.count).toEqual(42)
+  // })
 
-  it('Set close the votingdapp account', async () => {
-    // ARRANGE
-    expect.assertions(1)
-    const ix = getCloseInstruction({
-      payer: payer,
-      votingdapp: votingdapp.address,
-    })
+  // it('Set close the votingdapp account', async () => {
+  //   // ARRANGE
+  //   expect.assertions(1)
+  //   const ix = getCloseInstruction({
+  //     payer: payer,
+  //     votingdapp: votingdapp.address,
+  //   })
 
-    // ACT
-    await sendAndConfirm({ ix, payer })
+  //   // ACT
+  //   await sendAndConfirm({ ix, payer })
 
-    // ASSERT
-    try {
-      await fetchVotingdapp(rpc, votingdapp.address)
-    } catch (e) {
-      if (!isSolanaError(e)) {
-        throw new Error(`Unexpected error: ${e}`)
-      }
-      expect(e.message).toEqual(`Account not found at address: ${votingdapp.address}`)
-    }
-  })
+  //   // ASSERT
+  //   try {
+  //     await fetchVotingdapp(rpc, votingdapp.address)
+  //   } catch (e) {
+  //     if (!isSolanaError(e)) {
+  //       throw new Error(`Unexpected error: ${e}`)
+  //     }
+  //     expect(e.message).toEqual(`Account not found at address: ${votingdapp.address}`)
+  //   }
+  // })
 })
 
 // Helper function to keep the tests DRY
